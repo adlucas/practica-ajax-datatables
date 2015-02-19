b6b95bee20772490a05fff33f60aefae3d7e5fa3
angel <delucas91@gmail.com>
2015-02-19 18:43:11 +0100

    parte de edicion finalizada. Resfreco auto

--- app/php/modificar_clinica.php
+++ app/php/modificar_clinica.php
@@ -37,10 +37,35 @@ mysql_query('SET names utf8');
 $id = $_POST["idDoctor"];
 $nombre = $_POST["nombre"];
 $numcolegiado = $_POST["numcolegiado"];
-$nombre_clinica = $_POST["nombre_clinica"];
-$id_clinica = $_POST["id_clinica"];
+$clinicas = $_POST["id_clinica"];
 
-echo "id=$id nombre= $nombre colegiado= $numcolegiado";
+
+//echo "id=$id nombre= $nombre colegiado= $numcolegiado clinicas= $clinicas";
+
+$query = "delete from clinica_doctor where id_doctor=" . $id;
+$query_res = mysql_query($query);
+
+//echo"$query <br>";
+
+
+for ($i=0;$i<count($clinicas);$i++)    
+{     
+//echo "<br> CLinicaid " . $i . ": " . $clinicas[$i];   
+
+$query1 = "insert into clinica_doctor (id_doctor,id_clinica) values( 
+             ". $id . ", 
+            " . $clinicas[$i] . ")" ;
+
+
+//echo"$query1";
+
+
+
+            $query_res = mysql_query($query1);
+
+
+} 
+
 /* Consulta UPDATE */
 $query = "UPDATE doctores SET 
             nombre = '" . $nombre . "', 
@@ -66,5 +91,6 @@ $resultado = array();
       'mensaje' => $mensaje,
       'estado' => $estado
    );
+
 echo json_encode($resultado);
 ?>
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
    'use strict';
    $(document).ready(function() {
+      $('#example').show();
        var miTabla = $('#miTabla').DataTable({
            'processing': true,
            'serverSide': true,
@@ -34,18 +35,21 @@
                'data': 'numcolegiado'
            }, {
                'data': 'nombreClinica',
+                'render': function(data) {
+                   return '<li>' + data + '</li><br>';
+               }
            }, {
-               'data': 'idClinica'
+               'data': 'idClinica',
+                "visible": false
            }, {
                'data': 'idDoctor',
                /*añadimos las clases editarbtn y borrarbtn para procesar los eventos click de los botones. No lo hacemos mediante id ya que habrá más de un
                botón de edición o borrado*/
                'render': function(data) {
-                   return '<a class="btn btn-primary editarbtn" href=http://localhost/php/editar.php?id_doctor=' + data + '>Editar</a><a class="btn btn-warning borrarbtn" href=http://localhost/php/borrar_doctor.php?id_doctor=' + data + '>Borrar</a>';
+                   return '<a class="btn btn-primary editarbtn" href=http://localhost/php/modificar_clinica.php?id_doctor=' + data + '>Editar</a><a class="btn btn-warning borrarbtn" href=http://localhost/php/borrar_doctor.php?id_doctor=' + data + '>Borrar</a>';
                }
            }]
        });
-
        /*Creamos la función que muestre el formulario cuando hagamos click*/
        /*ojo, es necesario hacerlo con el método ON. Tanto por rendimiento como porque puede haber elementos (botones) que todavía no existan en el document.ready*/
        $('#miTabla').on('click', '.editarbtn', function(e) {
@@ -69,6 +73,9 @@
            var aData = miTabla.row(nRow).data();
            var idDoctor = aData.idDoctor;
 
+          
+           
+
 
            $.ajax({
                /*en principio el type para api restful sería delete pero no lo recogeríamos en $_REQUEST, así que queda como POST*/
@@ -82,25 +89,33 @@
                error: function(xhr, status, error) {
                    //mostraríamos alguna ventana de alerta con el error
                    alert("Ha entrado en error");
+                $('#edicionerr').html("Error al borrar doctor!").slideDown(2000).slideUp(2000);
+
                },
                success: function(data) {
+                alert("borrado ok");
                    //obtenemos el mensaje del servidor, es un array!!!
                    //var mensaje = (data["mensaje"]) //o data[0], en función del tipo de array!!
                    //actualizamos datatables:
                    /*para volver a pedir vía ajax los datos de la tabla*/
-                   miTabla.fnDraw();
+                   var $mitabla =  $("#miTabla").dataTable( { bRetrieve : true } );
+                  $mitabla.fnDraw();
+                   $('#edicionok').html("Borrado correcto!").slideDown(2000).slideUp(2000);
+
                },
                complete: {
                    //si queremos hacer algo al terminar la petición ajax
                }
            });
+
        });
        $('#enviar').click(function(e) {
            e.preventDefault();
            idDoctor = $('#idDoctor').val();
            nombre = $('#nombre').val();
            numcolegiado = $('#numcolegiado').val();
-          
+           id_clinica = $('#clinicas').val();
+
 
            $.ajax({
                type: 'POST',
@@ -113,23 +128,39 @@
                data: {
                    idDoctor: idDoctor,
                    nombre: nombre,
-                   numcolegiado: numcolegiado
+                   numcolegiado: numcolegiado,
+                   id_clinica:id_clinica
                    
                },
                error: function(xhr, status, error) {
                    //mostraríamos alguna ventana de alerta con el error
+                    alert(error);
+                    alert(xhr);
+
+                    alert(status);
+
+                    $('#edicionerr').slideDown(2000).slideUp(2000);
+
                },
                success: function(data) {
                   var $mitabla =  $("#miTabla").dataTable( { bRetrieve : true } );
                   $mitabla.fnDraw();
+                 // alert("ok");
+           		  $('#edicionok').slideDown(2000).slideUp(2000);
+
+                 
+
                },
                complete: {
                    //si queremos hacer algo al terminar la petición ajax
+
                }
            });
 
            $('#tabla').fadeIn(100);
            $('#formulario').fadeOut(100);
+            //$("#edicion").fadeOut(100);
+
 
        });
 
@@ -146,6 +177,7 @@
                //data: { id_clinica: id_clinica, nombre: nombre, ….,  id_tarifa: id_tarifa },
                error: function(xhr, status, error) {
                    //mostraríamos alguna ventana de alerta con el error
+              
                },
                success: function(data) {
                    $('#clinicas').empty();
@@ -154,6 +186,7 @@
                            $('<option></option>').val(this.id_clinica).html(this.nombre)
                        );
                    });
+
                },
                complete: {
                    //si queremos hacer algo al terminar la petición ajax
